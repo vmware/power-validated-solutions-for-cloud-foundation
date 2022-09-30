cefc93e76072c5a8de00907c21080c1492201d39
Ryan Johnson <johnsonryan@vmware.com>
2022-09-30 09:30:34 -0400

    ci(module): update basic module ci
    Update basic CI checks for module import and manifest check when changed are made to` .psm1` and `.psd1` only.
    
    Signed-off-by: Ryan Johnson <johnsonryan@vmware.com>

--- .ci/pester.tests.ps1
+++ .ci/pester.tests.ps1
@@ -1,18 +1,16 @@
-$moduleName = "PowerValidatedSolutions"
-
 BeforeAll {
-    Import-Module -Name "$PSScriptRoot/../$moduleName.psd1" -Force -ErrorAction Stop
+    Import-Module -Name "$PSScriptRoot/../PowerValidatedSolutions.psd1" -Force -ErrorAction Stop
 }
 
 Describe -Tag:('ModuleValidation') 'Module Baseline Validation' {
 
     It 'is present' {
-        $module = Get-Module $moduleName
+        $module = Get-Module PowerValidatedSolutions
         $module | Should -Be $true
     }
 
     It ('passes Test-ModuleManifest') {
-        Test-ModuleManifest -Path:("$PSScriptRoot/../$moduleName.psd1") | Should -Not -BeNullOrEmpty
+        Test-ModuleManifest -Path:("$PSScriptRoot/../PowerValidatedSolutions.psd1") | Should -Not -BeNullOrEmpty
         $? | Should -Be $true
     }
 }
