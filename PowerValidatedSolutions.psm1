d814f2554b21d760f2527c0086441c3fb50f49fa
Ryan Johnson <johnsonryan@vmware.com>
2023-04-24 10:05:23 -0400

    chore(deps): bump module prerequisites
    - Updates `VMware.PowerCLI` from v12.7.0 to v13.0.0.
    - Updates `PowerVCF` from v2.2.0 to v2.3.0.
    - Updates `VMware.vSphere.SsoAdmin` from v1.3.8 to v1.3.9.
    - Updates `ImportExcel` from v7.1.1 to v7.8.4.
    
    Signed-off-by: Ryan Johnson <johnsonryan@vmware.com>

--- PowerValidatedSolutions.psm1
+++ PowerValidatedSolutions.psm1
@@ -31635,7 +31635,7 @@ Function Test-PowerValidatedSolutionsPrereq {
             @{ Name=("VMware.PowerCLI"); MinimumVersion=("13.0.0")}
             @{ Name=("VMware.vSphere.SsoAdmin"); MinimumVersion=("1.3.9")}
             @{ Name=("ImportExcel"); MinimumVersion=("7.8.4")}
-            @{ Name=("PowerVCF"); MinimumVersion=("2.2.0")}
+            @{ Name=("PowerVCF"); MinimumVersion=("2.3.0")}
         )
 
         foreach ($module in $modules ) {
