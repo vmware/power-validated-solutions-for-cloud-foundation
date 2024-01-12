157c75df9e49378de9308b56d21ebdd43a84d6f3
Ryan Johnson <ryan.johnson@broadcom.com>
2024-01-12 18:52:46 -0500

    docs: documentation updates (#467)
    - Updates installation documentation for the next release.
    - Updates the MkDocs plugin requirements.
    - Updates the MkDocs configuration to include missing functions.
    
    Signed-off-by: Ryan Johnson <ryan.johnson@broadcom.com>

--- docs/snippets/install-module.ps1
+++ docs/snippets/install-module.ps1
@@ -3,4 +3,4 @@ Install-Module -Name VMware.PowerCLI -MinimumVersion 13.1.0 -Repository PSGaller
 Install-Module -Name VMware.vSphere.SsoAdmin -MinimumVersion 1.3.9 -Repository PSGallery
 Install-Module -Name PowerVCF -MinimumVersion 2.4.0 -Repository PSGallery
 Install-Module -Name ImportExcel -MinimumVersion 7.8.5 -Repository PSGallery
-Install-Module -Name PowerValidatedSolutions -MinimumVersion 2.7.0 -Repository PSGallery
+Install-Module -Name PowerValidatedSolutions -MinimumVersion 2.8.0 -Repository PSGallery
